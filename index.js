82cc801830fa4990b1ea1d60f4e6f2e64c164d89
Narsingh22 <k.narsingh22@gmail.com>
2025-03-01 00:31:16 +0530

    A small todo list

---
+++ index.js
@@ -0,0 +1,69 @@
+let myArr = [];
+let myStr = [];
+let tableBody = document.getElementById('tableBody');
+
+function getAndUpdate() {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    
+    if (localStorage.getItem('myTodoList')==null){      
+        myArr.push([title, description]);
+        localStorage.setItem('myTodoList', JSON.stringify(myArr));
+        
+    }
+    else {        
+        myStr = localStorage.getItem('myTodoList');
+        myArr = JSON.parse(myStr);
+        myArr.push([title, description]);
+        localStorage.setItem('myTodoList', JSON.stringify(myArr));
+    }
+    update();
+}
+
+function update() {
+    if (localStorage.getItem('myTodoList')==null){
+        myArr.splice(0);  
+    }
+    else {
+        myStr = localStorage.getItem('myTodoList');
+        myArr = JSON.parse(myStr);
+    }
+
+    let str = '';
+
+    myArr.forEach((element, index) => {
+      
+            str += `
+            <tr>
+                <th>${index + 1}</th>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td><button id="dlt" onclick="deleted(${index})">Delete</button></td>
+            </tr>`;
+       
+
+    });
+
+    tableBody.innerHTML = str;
+
+}
+
+let add = document.getElementById("add");
+add.addEventListener("click", getAndUpdate);
+
+
+function deleted(itemIndex) {
+    myStr = localStorage.getItem('myTodoList');
+    myArr = JSON.parse(myStr);
+    myArr.splice(itemIndex, 1);
+    localStorage.setItem('myTodoList', JSON.stringify(myArr));
+    update();
+}
+
+function clearStorage() {
+    if(confirm("Do you really want to clear?")){
+        localStorage.clear();
+        myArr.splice(0);
+        update();
+    }
+}
